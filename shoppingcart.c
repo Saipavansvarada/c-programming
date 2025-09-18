3c3408ed924462926203f5f0e29f96ea19230afd
SAI Pavan <Saipavan.s.varada@gmail.com>
2025-09-18 22:29:09 +0530

    new code

---
+++ shoppingcart.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+    char item[50]="";
+    float price=0.0f;
+    int quantity=0;
+    float total=0.0f;
+
+    printf("enter item name: ");
+    fgets(item, sizeof(item), stdin);
+
+    printf("enter price of each item: ");
+    scanf("%f", &price);
+    getchar(); 
+
+    printf("how many items you want to buy: ");
+    scanf("%d", &quantity);
+
+    total=price*quantity;
+    printf("your total bill is: %.2f", total);
+}
